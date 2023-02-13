afe5c89b2d439d7f61f93df0241633cfe15f1976
jonnypagliacci <dr.sergio.valdovinos@gmail.com>
2023-02-13 15:18:16 -0600

    Subo proyecto React con Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups"; 
+import FavoritePage from "./pages/favorites";
+import NewMeetupsPage from "./pages/NewMeetups";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div> 
+    <Routes>
+      <Route path="/" element = {<AllMeetupsPage />}></Route>
+      <Route path="/favorites" element = {<FavoritePage />}></Route>
+      <Route path="/NewMeetups" element = {<NewMeetupsPage />}></Route>
+    </Routes>
+  </div>;
 }
 
 export default App;
